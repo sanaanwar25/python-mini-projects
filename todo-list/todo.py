9d793b2ac029ee48b3f247902848f2f1a314252d
Sana Anvar Zadath <basithanwar14@gmail.com>
2026-02-02 04:14:42 +0400

    add delete task feature to to-do list

--- todo-list/todo.py
+++ todo-list/todo.py
@@ -19,11 +19,29 @@ def add_task():
     print("Task added successfully.")
 
 
+def delete_task():
+    show_tasks()
+    try:
+        task_no = int(input("Enter task number to delete: "))
+        with open("tasks.txt", "r") as file:
+            tasks = file.readlines()
+        if 1 <= task_no <= len(tasks):
+            tasks.pop(task_no - 1)
+            with open("tasks.txt", "w") as file:
+                file.writelines(tasks)
+            print("Task deleted.")
+        else:
+            print("Invalid task number.")
+    except:
+        print("Error deleting task.")
+
+
 while True:
     print("\nTo-Do List Menu")
     print("1. Show tasks")
     print("2. Add task")
-    print("3. Exit")
+    print("3. Delete task")
+    print("4. Exit")
 
     choice = input("Enter your choice: ")
 
@@ -32,8 +50,9 @@ while True:
     elif choice == "2":
         add_task()
     elif choice == "3":
+        delete_task()
+    elif choice == "4":
         print("Goodbye!")
         break
     else:
         print("Invalid choice. Try again.")
-
